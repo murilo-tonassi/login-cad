b9cfc8199718c49bfb307d39cc80248cc7368299
Murilo <murilo.tonassi@alunoescolassociais.org.br>
2024-07-04 07:35:48 -0300

    implementação do código JavaScript

--- controller.js
+++ controller.js
@@ -2,4 +2,10 @@
 function acessar(){
     let loginEmail = document.getElementByld('loginEmail').value;
     let loginSenha = document.getElementByld('loginSenha').value;
+
+    if(!loginEmail || !loginSenha){
+        alert('Favor preencher todos os campos');
+    }else{
+        alert('Campos preenchidos com sucesso');
+    }
 }
