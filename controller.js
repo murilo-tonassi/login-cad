18ad9d3d7c5ebed67e226db313f7e91796ebdd13
murilo-tonassi <murilo.tonassi@alunoescolassociais.org.br>
2024-08-08 08:04:58 -0300

    correção na função de criação de lista e criação de Array para armazenamento de nomes

--- controller.js
+++ controller.js
@@ -1,8 +1,8 @@
-// VALIDAÇÃPO DE PREENCHIMENTO
+// VALIDAÇÃO DE PREENCHIMENTO
 function acessar(){
     let loginEmail = document.getElementById('loginEmail').value;
     let loginSenha = document.getElementById('loginSenha').value;
-
+ 
     if(!loginEmail || !loginSenha){
         alert('Favor preencher todos os campos');
     }else{
@@ -10,28 +10,28 @@ function acessar(){
         window.location.href = 'cadastro.html';
     }
 }
-
+ 
 // FUNÇÃO DE CRIAÇÃO DE ARRAY PARA ARMAZENAMENTO DE NOMES
-
+ 
 var dadosLista = [];
-
+ 
 function salvarUser(){
     let nomeUser = document.getElementById('nomeUser').value;
-
     if(nomeUser){
-
+        dadosLista.push(nomeUser);
+        // console.log(dadosLista);
+        criaLista();
+        document.getElementById('nomeUser').value = "";
     } else{
-        alert("Favor, informar um nome");
+        alert("Favor, informar um nome para cadastro");
     }
-
 }
-
+ 
 // FUNÇÃO DE CRIAÇÃO DE LISTA
-function crialista(){
-    let tabela = document.getElementById('tabela')
-    innerHTML "<tr><th>Nome Usuário</th><th>Ações</th></tr>";
-
+function criaLista(){
+    let tabela = document.getElementById('tabela').innerHTML = "<tr><th>Nome Usuário</th><th>Ações</th></tr>";
     for(let i = 0; i <= (dadosLista.length - 1); i++){
         tabela += "<tr><td>" + dadosLista[i] + "</td><td></td></tr>";
+        document.getElementById('tabela').innerHTML = tabela;
     }
 }
